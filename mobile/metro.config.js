a442cfe6743897a45f809b0c25b7afe2ee7645fe
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-09-04 18:10:52 +0530

    Create metro.config.js

---
+++ mobile/metro.config.js
@@ -0,0 +1,34 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resolver: {
+    alias: {
+      '@': './src',
+      '@components': './src/components',
+      '@screens': './src/screens',
+      '@navigation': './src/navigation',
+      '@services': './src/services',
+      '@utils': './src/utils',
+      '@types': './src/types',
+      '@assets': './src/assets',
+      '@hooks': './src/hooks',
+    },
+  },
+  watchFolders: ['../shared'], // For monorepo setups
+};
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
