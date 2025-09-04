dc683a5160fa6fe0587784aa251e20cf036788c5
Bhanu Karnwal <74866394+bhanukaranwal@users.noreply.github.com>
2025-09-04 18:11:08 +0530

    Create babel.config.js

---
+++ mobile/babel.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@utils': './src/utils',
+          '@types': './src/types',
+          '@assets': './src/assets',
+          '@hooks': './src/hooks',
+        },
+      },
+    ],
+    'react-native-reanimated/plugin', // Should be last
+  ],
+};
